00ac3fc717c71d4ff68f999e4a19bc900d9664f5
PCCP1234 <147849952+PCCP1234@users.noreply.github.com>
2024-02-08 13:40:26 +0000

    add files

---
+++ src/pages/index.tsx
@@ -0,0 +1,118 @@
+import type { NextPage } from "next";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+import Image from "next/image";
+import { SignInButton, useUser } from "@clerk/nextjs";
+import { api } from "~/utils/api";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
+import { PageLayout } from "~/components/layout";
+import { PostView } from "~/components/postview";
+
+
+
+const CreatePostWizard = () => {
+  const { user } = useUser();
+
+  const [input, setInput] = useState("");
+
+  const ctx = api.useUtils();
+
+  const { mutate, isLoading: isPosting } = api.post.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      void ctx.post.getAll.invalidate();
+    },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors?.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to post! Please try again later.");
+      }
+    },
+  });
+
+  if (!user) return <div/>;
+
+  return (
+    <div className="flex w-full gap-3 ">
+      <Image src={user.imageUrl}
+        alt="Profile image"
+        className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
+      />
+      <input
+        placeholder="Type some emojis!"
+        className="grow bg-transparent outline-none"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
+        disabled={isPosting}
+      />
+      {input !== "" && !isPosting && (
+        <button onClick={() => mutate({ content: input })}>Post</button>
+      )}
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+const Feed = () => {
+
+  const { data, isLoading: postsLoading } = api.post.getAll.useQuery();
+
+  if (postsLoading) return <LoadingPage />;
+
+  if (!data) return <div>Something went wrong</div>;
+
+  return (
+    <div className="flex flex-col">
+      {data.map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
+      ))}
+    </div>
+  );
+};
+
+const Home: NextPage = () => {
+
+  const { isLoaded: userLoaded, isSignedIn } = useUser();
+
+  //Start fetching asap
+  api.post.getAll.useQuery();
+
+
+  //Return empty div if user isn´t loaded
+  if (!userLoaded) return null;
+
+  return (
+    <PageLayout>
+      <div className="flex border-b border-slate-400 p-4">
+        {!isSignedIn && (
+          <div className="flex justify-center">
+            <SignInButton />
+          </div>
+        )}
+        {isSignedIn && <CreatePostWizard />}
+      </div>
+      <Feed />
+    </PageLayout>
+
+  );
+}
+
+export default Home;
---
+++ src/server/api/routers/post.ts
@@ -0,0 +1,116 @@
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis"; // see below for cloudflare and fastly adapters
+import { filtUserForClient } from "~/server/helpers/filterUserForClient";
+import type { Post } from "@prisma/client";
+import { clerkClient } from "@clerk/nextjs";
+
+const addUserdataToPosts = async (posts: Post[]) => {
+  const users = (
+    await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    })
+  ).map(filtUserForClient);
+
+
+  return posts.map((post) => {
+    const author = users.find((user) => user.id === post.authorId);
+
+    if (!author || !author.username)
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Author for post not found",
+      });
+
+    return {
+      post,
+      author: {
+        ...author,
+        username: author.username,
+      },
+    };
+  });
+}
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  prefix: "@upstash/ratelimit",
+});
+
+
+export const postRouter = createTRPCRouter({
+
+  getById: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({
+      ctx, input }) => {
+      const post = await ctx.db.post.findUnique({
+        where: { id: input.id }
+      });
+
+      if (!post) throw new TRPCError({ code: "NOT_FOUND" });
+      
+      return (await addUserdataToPosts([post]))[0];
+    }),
+
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+      orderBy: [
+        { createdAt: "desc" }
+      ],
+    });
+
+    return addUserdataToPosts(posts);
+  }),
+
+  getPostsByUserId: publicProcedure
+    .input(
+      z.object({
+        userId: z.string()
+      })).query(async ({ ctx, input }) => ctx.db.post.findMany({
+        where: {
+          authorId: input.userId,
+        },
+        take: 100,
+        orderBy: [{ createdAt: "desc" }],
+      }).then(addUserdataToPosts)
+      ),
+
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().emoji("Only emojis are allowed!").min(1).max(280),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+
+      const authorId = ctx.userId;
+
+      const { success } = await ratelimit.limit(authorId);
+
+      if (!success)
+        throw new TRPCError({
+          code: "TOO_MANY_REQUESTS"
+        });
+
+      const post = await ctx.db.post.create({
+        data: {
+          authorId,
+          content: input.content,
+        }
+      });
+
+      return post;
+    }),
+});
